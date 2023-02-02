a77b30aadad6826f4113fb3c8f580510060f3140
serahrashmi <serahrashmi@gmail.com>
2023-02-02 22:16:02 +0100

    Pseudocode for area of rectangle
    Pseudocode to print area of the rectangle - Task2

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task2():
-    return TODO("Erase this line and write your solution")
+    return
+"""
+INPUT length of the rectangle 
+INPUT breath of the rectangle
+use the equation "Area = length * breath" to calculate the area
+Output: Area of the rectangle 
+
+Length = input (length of the rectangle)
+Breadth = input (breath of the rectangle)
+Area = (Length * Breadth)
+Print (Area)
+"""
