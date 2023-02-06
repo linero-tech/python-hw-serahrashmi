19b0e76440e6a765ce8f659d86e0445d475c5eae
serahrashmi <serahrashmi@gmail.com>
2023-02-06 20:49:38 +0100

    Solution for Task 7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,4 +2,15 @@ from to_do import TODO
 
 
 def task7():
-    return TODO("Erase this line and write your solution")
+    return
+
+
+"""
+START
+INPUT numbers from 1 and 100, inclusive
+CHECK each number
+    IF the number is perfectly DIVISIBLE by 2
+    PRINT(number)
+CONTINUE checking until 100 is reached
+END
+"""
