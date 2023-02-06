7069c171dca6e327c2f29dcf7af3b0fb35207433
serahrashmi <serahrashmi@gmail.com>
2023-02-06 20:50:22 +0100

    Solution for Task 8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,4 +2,20 @@ from to_do import TODO
 
 
 def task8():
-    return TODO("Erase this line and write your solution")
+    return
+
+
+"""
+START
+INPUT x 
+    IF x is perfectly divisible by 3
+    THEN Print (Foo)
+    IF x is perfectly divisible by 5
+    THEN Print (Bar)
+    IF x is perfectly divisible by 15
+    THEN Print (FooBar) 
+    IF x is divisible by 3 or 5 
+    THEN Print (Qix) 
+END 
+
+"""
