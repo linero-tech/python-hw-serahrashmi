6f5ba72d454c15cb833378fad9673c0600057809
serahrashmi <serahrashmi@gmail.com>
2023-02-06 21:02:49 +0100

    Solution for Task 11

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return
+
+
+"""
+INPUT temperature is 20C or 30C
+IF the readings are in Celcius
+    use the equation (temperature in celcius *9/5)+32
+    convert 
+    Print (Farenheit)
+ELSE temperature is Fahrenheit
+    use the equation (temperature in Farenheit _32)* 5/9 
+    convert
+    Print (Celcius)
+"""
