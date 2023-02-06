df027c87776e05de167a4f1f8a3e3ec46719e2e5
serahrashmi <serahrashmi@gmail.com>
2023-02-06 20:52:00 +0100

    Solution for Task 10

--- src/lms126/task10.py
+++ src/lms126/task10.py
@@ -2,4 +2,21 @@ from to_do import TODO
 
 
 def task10():
-    return TODO("Erase this line and write your solution")
+    return
+
+
+"""
+    Open Gmail on the web
+    Click login to Gmail
+    Click Register or Sign Up 
+    Choose a username using a character alphabet, numerical or special character
+    Check for availability 
+    Create a password with more than 6 characters it can be a alphabet, numerical 
+    or special character 
+    Confirm the password typing the same again 
+IF  the password matches 
+    create account successfully 
+ELSE reconfirm password 
+END
+
+"""
