c668d0092ed4b67d52f214b6e653b9ffeb4fb0c4
serahrashmi <serahrashmi@gmail.com>
2023-02-06 19:10:23 +0100

    Task 6 solution

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,4 +2,14 @@ from to_do import TODO
 
 
 def task6():
-    return TODO("Erase this line and write your solution")
+    return
+
+
+"""
+
+INPUT for number is 1 to 100
+    divide every number from 1 to 100 by 5
+IF number is completely divisible by 5 print number 
+ELSE End 
+
+"""
