fb18fc5448877f83aa56803afc7425d5a265d396
serahrashmi <serahrashmi@gmail.com>
2023-02-06 20:51:20 +0100

    Solution for Task 9

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,28 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return
+
+
+"""
+INPUT 
+IF the ATM is out of service 
+   THAN cant access ATM
+ELSE Continue
+    Insert the card in the slot 
+    select the language on the screen
+    select the option as withdrawal 
+    Enter the amount to be withdrawn
+    Enter the four digit PIN 
+    Confirm the amount - (Step Continue)
+    IF the PIN is correct
+    IF amount is léss than or equal to the account balance  
+    THEN release the money 
+    ELSE No balance
+    Print receipt 
+    ELSE 
+    PRINT(Incorrect PIN)
+END 
+"""
+
+
