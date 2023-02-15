bc04eb935a6c9745abb006ba527ddec6fb9de80b
KALAIDATA23 <vanimfa@gmail.com>
2023-02-15 14:35:00 +0100

    Update task5.py

--- src/lms127/task5.py
+++ src/lms127/task5.py
@@ -5,8 +5,7 @@ def task5(value_for_a, value_for_b):
     a = value_for_a
     b = value_for_b
 
-    TODO() print( value_for_a = value_for_b)
-    print(b=a)
+    TODO() ("write solution here")
 
     # Do not erase or change the below statement
     return a, b
