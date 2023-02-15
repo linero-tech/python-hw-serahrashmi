52983195dfd3761e78f4650f9902633d9e2c1b8b
Serah <serras1223@edu.lerum.se>
2023-02-15 19:22:12 +0100

    Update task3.py

--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -3,9 +3,9 @@ from to_do import TODO
 
 def task3(radius):
     return (result)
-radius = "2.0"
+radius = 2.0
 result = 2*3.14*2.0
 print(result)
 if __name__ == "__main__":
     task3(radius)
-    
+
