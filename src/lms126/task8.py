ed6b76bec6dd39e06a675eda8949c5759621f023
Serah <serras1223@edu.lerum.se>
2023-02-16 13:55:42 +0100

    updated task 7 & 8

--- src/lms126/task8.py
+++ src/lms126/task8.py
@@ -2,20 +2,7 @@ from to_do import TODO
 
 
 def task8():
-    return """
-START
-INPUT x 
-    IF x is perfectly divisible by 3
-    THEN Print (Foo)
-    IF x is perfectly divisible by 5
-    THEN Print (Bar)
-    IF x is perfectly divisible by 15
-    THEN Print (FooBar) 
-    IF x is not divisible by 3 or 5 
-    THEN Print (Qix) 
-END 
-
-"""
+    return 
 """
 1. Input x
 2. IF x is multiple of both 3 and 5 
