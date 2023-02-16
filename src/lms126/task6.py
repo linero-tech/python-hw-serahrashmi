5a8f6096ec0f60e5634e54e5f5d66c0bbbcb89ff
Serah <serras1223@edu.lerum.se>
2023-02-16 13:51:00 +0100

    updated task 6

--- src/lms126/task6.py
+++ src/lms126/task6.py
@@ -2,18 +2,10 @@ from to_do import TODO
 
 
 def task6():
-    return """
-START
-INPUT numbers from 1 and 100, inclusive
-CHECK each number
-    IF the number is perfectly DIVISIBLE by 5 
-    PRINT(number)
-CONTINUE checking until 100 is reached
-END
-"""
+    return
 """
 1. Input numbers from 1 to 100 
-2. divide each number by 5 
+2. Divide each number by 5 
 3. IF the number is perfectly divisible 
     print the number  
 4. continue until number 100
