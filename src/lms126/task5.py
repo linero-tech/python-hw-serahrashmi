585dfb751cfbf59b0253fecfbdd07bbe4fb6f623
Serah <serras1223@edu.lerum.se>
2023-02-16 13:52:57 +0100

    updated task 5

--- src/lms126/task5.py
+++ src/lms126/task5.py
@@ -2,36 +2,7 @@ from to_do import TODO
 
 
 def task5():
-    return """
-INPUT x
-OUTCOME = day of the week 
-
-IF X >/= 8
- THEN divide the number by the total days of the week, the reminder corresponds 
-  to the day of the week 
-   
-IF X=1
-    Print(Monday)
-    END
-IF X=2
-    Print(Tuesday)
-    END
-IF X=3
-    Print(Wednesday)
-    END
-IF X=4
-    Print(Thursday)
-    END
-IF X=5
-    Print(Friday)
-    END
-IF X=6
-    Print(Saturday)
-    END
-IF X=7
-    Print(Sunday)
-    END
-"""
+    return
 """
 1. IF x=1 
     print Monday
@@ -40,9 +11,7 @@ IF X=7
     ...
 3. ELSE IF x=8
     print Error 
-
-***
-
+"""
 
 
 
