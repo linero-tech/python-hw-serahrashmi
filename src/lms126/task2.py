fb4795cd47b9f5e150812329eae3da976f027a98
Serah <serras1223@edu.lerum.se>
2023-02-16 13:46:32 +0100

    task 2 126

--- src/lms126/task2.py
+++ src/lms126/task2.py
@@ -3,21 +3,11 @@ from to_do import TODO
 
 def task2():
     return """
-INPUT length of the rectangle 
-INPUT breath of the rectangle
-use the equation "Area = length * breath" to calculate the area
-Output: Area of the rectangle 
-
-Length = input (length of the rectangle)
-Breadth = input (breath of the rectangle)
-Area = (Length * Breadth)
-Print (Area)
-"""
-
-"""
 
 1. Input length of the triangle
 2. Input breadth of the triangle
 3. calculate the Area of the triangle 
 4. use the equation Area = length * breadth 
 5. print the Area
+
+"""
--- src/lms127/task3.py
+++ src/lms127/task3.py
@@ -2,10 +2,13 @@ from to_do import TODO
 
 
 def task3(radius):
-    return (result)
-radius = 2.0
-result = 2*3.14*2.0
-print(result)
+    radius = 2.0
+    result = 2 * 3.1416 * radius
+    return result
+
 if __name__ == "__main__":
-    task3(radius)
+    print(task3(radius = 2.0))
+    print(task3(radius =10.0))
+    print(task3(radius =25.5))
+
 
