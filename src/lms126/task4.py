b6e3abe58de9b64f981343317a164f1c8cd10eb6
Serah <serras1223@edu.lerum.se>
2023-02-16 13:49:30 +0100

    updated task 4

--- src/lms126/task4.py
+++ src/lms126/task4.py
@@ -2,25 +2,11 @@ from to_do import TODO
 
 
 def task4():
-    return """
-START
-INPUT x 
-INPUT y
-OUTCOME
-IF x=y
-    OUTCOME "Equal"
-    END
-IF x>y
-    OUTCOME = x
-ELSE 
-    OUTCOME = y
-print(OUTCOME)
-END
-"""
+    return
 """
 1. Input the number x 
 2. Input the value y 
-3. if x is greater than y 
+3. IF x is greater than y 
    print x
-4. else print y 
+4. ELSE print y 
 """
