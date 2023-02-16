91a9ca92e202c2b84db3b149c0ce8246117f124c
Serah <serras1223@edu.lerum.se>
2023-02-16 13:54:39 +0100

    updated task 7

--- src/lms126/task7.py
+++ src/lms126/task7.py
@@ -2,15 +2,7 @@ from to_do import TODO
 
 
 def task7():
-    return """
-START
-INPUT numbers from 1 and 100, inclusive
-CHECK each number
-    IF the number is perfectly DIVISIBLE by 2
-    PRINT(number)
-CONTINUE checking until 100 is reached
-END
-"""
+    return 
 """
 1. Input numbers from 1 to 100
 2. IF the number is divisible by 2 
