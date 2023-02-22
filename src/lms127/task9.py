eabfd745d2c9eaefcd36f21336b9c3eee8529f22
Serah <serras1223@edu.lerum.se>
2023-02-22 11:43:33 +0100

    Changes to task9

--- src/lms127/task9.py
+++ src/lms127/task9.py
@@ -2,7 +2,9 @@ from to_do import TODO
 
 
 def task9(sentence, character):
-    result = sentence.lower().count(character.lower())
+    lower_sentence = sentence.lower()
+    lower_character = character.lower()
+    result = lower_sentence.count(lower_character)
     if result > 0:
         return "true"
     else:
@@ -10,5 +12,5 @@ def task9(sentence, character):
 
 
 if __name__ == "__main__":
-    print(task9("I code in KOTLIN", "I"))
-    print(task9("summer is beautiful", "y"))
+    print(task9("I code In KOTLIN", "I"))
+    print(task9("summer is beautiful", "I"))
