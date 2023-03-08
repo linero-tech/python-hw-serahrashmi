fc9ee88c3b4a6872c6c496f6fe4990e8c2278ec1
Serah <serras1223@edu.lerum.se>
2023-03-08 17:00:06 +0100

    task 3

--- src/lms129/task3.py
+++ src/lms129/task3.py
@@ -5,10 +5,11 @@ def task3(number):
     result = 1
     initial_number = 1
     while initial_number <= number:
-        initial_number = initial_number * 1
+        result = result * initial_number
+        initial_number = initial_number + 1
     return result
 
 
 if __name__ == "__main__":
-    print(task3(5))
+    print(task3(3))
     print(task3(10))
